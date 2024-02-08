2f3a10ceba8d9ec774fc413cdef31b1ab06a1876
AbbasKothari1552 <ask50405808@gmail.com>
2024-02-08 21:44:20 +0530

    subtration feature

--- sum.cpp
+++ sum.cpp
@@ -4,11 +4,17 @@ using namespace std;
 int addition(int a,int b) {
     return a + b;
 }
+int subtraction(int a,int b) {
+    return a - b;
+}
 
 int main() {
     int a = 10;
     int b = 20;
     int sum = addition(a,b);
+    int sub = subtraction(b,a);
     cout<<"Addition is:"<<sum<<endl;
+    cout<<"Subtraction is:"<<sub<<endl;
+
     return 0;
 }
